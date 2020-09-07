705e77326c17eb849e3754020f799bff3d81d3fa
Jada Arnett <jada.arnett@gmail.com>
2020-09-06 20:09:26 -0400

    added button grid spacing and started js, tested start game function using start button

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+var startButton = document.querySelector("#start-btn");
+
+
+startButton.addEventListener("click", startQuiz)
+
+
+function startQuiz() {
+    console.log("you pressed the start button.")
+    
+}
+
+
+function setNextQuestion() {
+
+
+}
+
+
+function selectAnswer() {
+
+}
